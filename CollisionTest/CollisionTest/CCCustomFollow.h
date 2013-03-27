c693e1459de919ec5b880665d3431cb44071e330
lizerspockzebrafish <llsaphire1029@gmail.com>
2013-03-26 23:03:03 -0500

    Created CCCustomFollow, corrected the parallax background bug, fix(hopefully) the irresponsive tapping

---
+++ CollisionTest/CollisionTest/CCCustomFollow.h
@@ -0,0 +1,16 @@
+//
+//  CCCustomFollow.h
+//  CollisionTest
+//
+//  Created by Lan Li on 3/26/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface CCCustomFollow : CCCustomFollow {
+    
+}
+
+@end
---
+++ CollisionTest/CollisionTest/CCCustomFollow.mm
@@ -0,0 +1,14 @@
+//
+//  CCCustomFollow.m
+//  CollisionTest
+//
+//  Created by Lan Li on 3/26/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "CCCustomFollow.h"
+
+
+@implementation CCCustomFollow
+
+@end
