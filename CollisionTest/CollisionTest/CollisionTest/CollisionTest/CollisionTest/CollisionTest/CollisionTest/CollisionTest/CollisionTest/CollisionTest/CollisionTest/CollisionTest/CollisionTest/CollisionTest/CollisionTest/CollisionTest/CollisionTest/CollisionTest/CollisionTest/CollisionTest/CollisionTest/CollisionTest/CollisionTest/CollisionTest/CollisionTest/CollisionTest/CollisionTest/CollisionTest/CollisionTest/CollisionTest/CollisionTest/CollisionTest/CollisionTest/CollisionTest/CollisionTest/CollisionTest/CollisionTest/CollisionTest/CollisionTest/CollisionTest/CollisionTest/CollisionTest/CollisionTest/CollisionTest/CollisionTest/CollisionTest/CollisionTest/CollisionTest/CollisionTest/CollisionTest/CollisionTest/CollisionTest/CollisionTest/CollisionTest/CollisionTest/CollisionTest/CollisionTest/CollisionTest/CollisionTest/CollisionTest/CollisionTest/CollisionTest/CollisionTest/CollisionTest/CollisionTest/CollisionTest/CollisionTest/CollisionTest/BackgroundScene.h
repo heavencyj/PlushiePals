6d03f59f4e7ea18daf431bbe47101dd4508d265b
heavencyj <yc15rice@gmail.com>
2013-04-10 22:47:38 -0500

    git is weird

---
+++ CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/CollisionTest/BackgroundScene.h
@@ -0,0 +1,16 @@
+//
+//  BackgroundScene.h
+//  CollisionTest
+//
+//  Created by Lan Li on 3/15/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface BackgroundScene : CCLayer {
+
+}
+-(id) init;
+@end
