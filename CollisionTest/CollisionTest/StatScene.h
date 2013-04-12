c038a7bbedec9700c8ef03678f886aa3616e31db
heavencyj <yc15rice@gmail.com>
2013-04-12 01:43:03 -0500

    updates on new navigtaion for stats and also loading screen

---
+++ CollisionTest/CollisionTest/StatScene.h
@@ -0,0 +1,13 @@
+//
+//  StatScene.h
+//  CollisionTest
+//
+//  Created by Heaven Chen on 4/12/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StatScene : NSObject
+
+@end
---
+++ CollisionTest/CollisionTest/StatScene.mm
@@ -0,0 +1,13 @@
+//
+//  StatScene.m
+//  CollisionTest
+//
+//  Created by Heaven Chen on 4/12/13.
+//
+//
+
+#import "StatScene.h"
+
+@implementation StatScene
+
+@end
