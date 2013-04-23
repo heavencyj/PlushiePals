667c8890ad35f0cb1ff46c67d508feeb7f6496e3
heavencyj <yc15rice@gmail.com>
2013-04-23 02:54:08 -0500

    add plushies and backstory

---
+++ CollisionTest/CollisionTest/BackStoryScene.h
@@ -0,0 +1,13 @@
+//
+//  BackStoryScene.h
+//  CollisionTest
+//
+//  Created by Heaven Chen on 4/23/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BackStoryScene : NSObject
+
+@end
---
+++ CollisionTest/CollisionTest/BackStoryScene.mm
@@ -0,0 +1,13 @@
+//
+//  BackStoryScene.m
+//  CollisionTest
+//
+//  Created by Heaven Chen on 4/23/13.
+//
+//
+
+#import "BackStoryScene.h"
+
+@implementation BackStoryScene
+
+@end
